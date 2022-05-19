1c9fbaec09540b9fb1efeacb21b9296030116d1c
tjm-teo <105624091+tjm-teo@users.noreply.github.com>
2022-05-19 12:28:18 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css';
+import Stats from './Stats.js'
+
+function App() {
+  return (
+    <>
+    <Stats/>
+    <button> First Choice</button>
+    <button> Second Choice</button>
+    <button> Third Choice</button>
+    <div> Make a Decision</div>
+    </>
+  )
+}
+
+export default App;
